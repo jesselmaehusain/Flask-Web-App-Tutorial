b7fa90702b4151e05f688eb7d074f9bd736bd0d0
Jessel Mae Husain <202180287@psu.palawan.edu.ph>
2026-05-29 19:37:50 +0800

    added notes using postman POST Method

--- website/api.py
+++ website/api.py
@@ -21,3 +21,28 @@ def get_notes():
         })
 
     return jsonify(result), 200
+
+
+@api.route('/notes', methods=['POST'])
+def create_note():
+
+    data = request.get_json()
+
+    if not data or not data.get("data"):
+            return jsonify({
+                "error": "Note data is required"
+            }), 400
+
+    new_note = Note(
+        data=data["data"],
+        user_id=1
+    )
+
+    db.session.add(new_note)
+    db.session.commit()
+
+    return jsonify({
+        "message": "Note created successfully",
+        "id": new_note.id,
+        "data": new_note.data
+    }), 201
